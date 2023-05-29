a7adf113f618e7946ee62251e0bbe795168c9e25
jobmania <xzxz7003@naver.com>
2023-05-29 23:20:10 +0900

    스트림생성 방법 3가지.
    1. // 1. Arrays.stream -배열 
    2.    // 2. Collection.stream - colleciton
    3.    // 3. Stream.of() - 직접 값 주입

--- src/chap_10/_05_Stream.java
+++ src/chap_10/_05_Stream.java
@@ -1,11 +1,33 @@
 package chap_10;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class _05_Stream {
 
     public static void main(String[] args) {
-        // 스트림
+        // 1. Arrays.stream
+        int[] scores = {100, 95, 90, 85, 80};
+        IntStream scoreStream = Arrays.stream(scores);
+
+
+        String[] langs = {"python", "java", "c","c++"};
+        Stream<String> langStream = Arrays.stream(langs);
+
+        // 2. Collection.stream
+        List<String> langList = Arrays.asList("python", "java", "c", "c++");
+        Stream<String> langListStream = langList.stream();
+
+        // 3. Stream.of()
+        Stream<String> streamList = Stream.of("python", "java", "c", "c++");
 
 
 
+        // 스트림사용 ㄱㄱㄱ
+        // 중간연산(Intermediate Operation) : filter,
+        // 최종연산(Terminal Operation)
     }
 }
